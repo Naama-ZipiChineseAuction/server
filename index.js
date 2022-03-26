1c191c3a0b6c0ef61f2cb55dcd9257b94185e13a
ZipiWerner <zipi100100@gmail.com>
2022-03-27 00:39:27 +0300

    addChangeToIndex

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const users = require('./controllers/users')
+const products=require("./controllers/products")
+const winnings=require("./controllers/winning")
 const app = express()
 app.use(express.json())
 app.use(cors())
 // controllers
 //app.use(product)
 app.use(users)
+app.use(products)
+app.use(winnings)
 app.get('/', async function (req, res) {
     res.send("ברוך הבא למכירה הסינית")
 })
