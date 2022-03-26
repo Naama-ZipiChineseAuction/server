74ddc8950f89db7745ddddb1ad6c9f1121d85284
naamafrank1 <naama0556772275@gmail.com>
2022-03-26 23:33:10 +0300

    functionGetAndPostInUserController

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require('express')
 const cors = require('cors')
-const product = require('.controllers/product')
-const users = require('.controllers/users')
+const users = require('./controllers/users')
 const app = express()
-
-
 app.use(express.json())
 app.use(cors())
-
 // controllers
-app.use(product)
+//app.use(product)
 app.use(users)
-
-
+app.get('/', async function (req, res) {
+    res.send("ברוך הבא למכירה הסינית")
+})
 app.listen(4500, () => console.log(`listening at http://localhost:4500`));
 
