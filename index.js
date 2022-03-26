5ac8bf26c36942c6b32168796ddb0ecdd69cbd10
ZipiWerner <zipi100100@gmail.com>
2022-03-26 23:56:39 +0300

    addController

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const cors = require('cors')
+const users = require('./controllers/users')
 const product = require('./controllers/products')
-const winning=require('./controllers/winning')
-//const users = require('.controllers/users')
+ const winning=require('./controllers/winning')
 const app = express()
-
-
 app.use(express.json())
 app.use(cors())
 
-// controllers
+app.use(users)
 app.use(product)
 app.use(winning)
-//app.use(users)
 app.get('/', async function (req, res) {
-    res.status(200).send("ברוכים הבאים למכירה הסינית של ציפי ונעמה!!!!!!!!!!!")
+    res.send("ברוך הבא למכירה הסינית")
 })
 app.listen(4500, () => console.log(`listening at http://localhost:4500`));
+
+
+
+
+
 
