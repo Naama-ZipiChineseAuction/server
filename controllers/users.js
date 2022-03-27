d622a2308a7ecf376643fa736d8b65f012fcac91
naamafrank1 <naama0556772275@gmail.com>
2022-03-27 19:21:33 +0300

    doControlAndRouterInUserAndOrder

--- controllers/users.js
+++ controllers/users.js
@@ -1,21 +1,70 @@
-const express = require('express')
-const router = express.Router()
-const user=require('../models/user.model')
 
-router.get('/users/getProduct/:id', async function (req, res) {
-    res.send("get product")
-})
-router.post('/users/addUser', async function (req, res) {    
-    const userAdd = req.body
-    if (!userAdd) {
-        res.status(400).send("user not found")
-        return
+
+const { User } = require("../models/user.model");
+const { orderSchema } = require("../models/order.model");
+
+module.exports.addUser = async (req, res) => {
+    let userAdd = req.body;
+
+    try {
+        let a=new User(userAdd)
+         await a.save()
+         res.status(201).send("user added")    
+
     }
-   let a=new User(userAdd)
-   await a.save()
-    res.status(201).send("user added")
-})    
-router.post('/users/addOrder/id', async function (req, res) {
-res.status(200).send("order added!!")
-})
-module.exports = router;
+    catch (e) {
+        console.log(e);
+        return res.status(400).send(e);
+    }
+
+}
+module.exports.getProductById=async(req,res)=>{
+    try {
+     let id = req.params.id;
+    let order = await User.findById(id)
+    order=order.arr_orders
+    return res.send(order); 
+
+    }
+    catch (e) {
+        console.log(e);
+        return res.status(400).send(e);
+    }
+}
+module.exports.getAllUser=async(req,res)=>{
+    try {
+   
+    let user = await User.find({ });
+    return res.send(user); 
+
+    }
+    catch (e) {
+        console.log(e);
+        return res.status(400).send(e);
+    }
+}
+module.exports.addOrder = async (req, res) => {
+    let user = req.body;
+    let id=user._id;
+    let x = await User.findById(id)
+    if(x==null)
+   {
+    let userAdd = user
+
+    try {
+        let a=new User(userAdd)
+         await a.save()
+        return res.status(201).send("user added")    
+
+    }
+    catch (e) {
+        console.log(e);
+        return res.status(400).send(e);
+    }
+   }
+   else{
+    x.arr_orders.push(user.arr_orders)
+   return res.status(201).send("add order to user")  
+   }
+
+}
